62a766119904c428329d2bde7f0a3075b580283b
ConstantinPonf <44893335+ConstantinPonf@users.noreply.github.com>
2018-11-09 22:51:06 +0100

    test

---
+++ arduinoCom/arduinoAPI.py
@@ -0,0 +1,27 @@
+import serial
+import time
+import json
+
+
+ser = serial.Serial('COM3', 9600,timeout = 1000)
+
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
+
+
+# Example
+data = {}
+data["triggered"] = 'false'
+
+writeToJSONFile('./','arduinoState',data)
+
+
+while True:
+    input = ser.readline()
+    if "TRIGGER" in input.decode("utf-8"):
+        data["triggered"] = "true"
+        writeToJSONFile('./','arduinoState',data)
+        break
+
