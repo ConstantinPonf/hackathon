874d0d1570125783a299217b05951f37f823cc16
ConstantiPonf <constantin.ponfick@yahoo.de>
2018-11-10 07:27:30 +0100

    pythonScript

--- coffee/src/app/coffee-information/resources/arduino.py
+++ coffee/src/app/coffee-information/resources/arduino.py
@@ -9,44 +9,24 @@ ser = serial.Serial('COM3', 9600,timeout = 1000)
 triggered = False
 recievedTrigger = False
 
-
-def writeToJSONFile(path, fileName, data):
-    filePathNameWExt = './' + path + '/' + fileName + '.json'
-    with open(filePathNameWExt, 'w') as fp:
-        json.dump(data, fp)
-
-def getFromJSONFile(path,fileName):
-    filePathNameWExt = './' + path + '/' + fileName + '.json'
-    with open(filePathNameWExt) as f:
-        data = json.load(f)
-        return data
-
 def request():
     f = urllib.request.urlopen("http://localhost:8081/test")
     print(f.read().decode("utf-8"))
 
-data = {}
-data["triggered"] = 'false'
-data["recievedTrigger"] = "false"
-
-writeToJSONFile('./','arduinoState',data)
 request()
 
 while True:
     input = ser.readline()
-    data = getFromJSONFile('./','arduinoState')
     
-    if ("TRIGGER" in input.decode("utf-8") )and data["triggered"] == 'false' and data["recievedTrigger"] == "false":
-        data["triggered"] = "true"
+    if ("TRIGGER" in input.decode("utf-8") )and triggered and not recievedTrigger:
+        triggered = True
         ## not true in final version
-        data["recievedTrigger"] = "true"
-        print(data)
-        writeToJSONFile('./','arduinoState',data)
+        recievedTrigger = False
+        print(triggered)
+        print(recievedTrigger)
         
     
-    if data["triggered"] == 'true' and data["recievedTrigger"] == "true":
-        data["triggered"] = "false"
-        data["recievedTrigger"] = "false"
-        print(data)
-        writeToJSONFile('./','arduinoState',data)
+    if triggered and recievedTrigger:
+        triggered = False
+        recievedTrigger = False
         ser.write(b'1')
