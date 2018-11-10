a817b26a1fd05055015d085c6cfe50e8e69f1db1
ConstantiPonf <constantin.ponfick@yahoo.de>
2018-11-10 06:54:49 +0100

    server

--- coffee/src/app/coffee-information/resources/server.js
+++ coffee/src/app/coffee-information/resources/server.js
@@ -1,5 +1,4 @@
-ipfsService = require('./services/ipfsService');
-contractService = require('./services/contractService');
+
 var cors = require('cors');
 var express = require('express');
 var multer = require('multer');
@@ -9,49 +8,25 @@ app.use(cors())
 
 
 // opens IPFS node
-ipfsService.node.on('ready', () => {
+
+
+var cors = require('cors');
+var express = require('express');
+var multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
+var app = express();
+app.use(cors())
+
 
   // opens express listener
   app.listen(8080, function () {
     console.log('App listening on port 8080!');
   });
 
-  // sets http post listen
-  app.post("/data", upload.single("file"), (req, res) => {
-    ipfsService.uploadFile(req.file).then(result => {
-      //console.log(result)
-      dataJSON = JSON.parse(req.body.data)
-      let data = {
-        hash: result[0].hash,
-        name: dataJSON.fileName,
-        description: dataJSON.description,
-      }
-      console.log(data)
-      contractService.writeData(data).then(resolve => { res.send(resolve) }, err => { res.send(err) })
-    })
+  app.get("/test", (req, res) => {
+    console.log(req.query)
+    res.send("Läuft")
   });
 
 
-  // sets http post listen
-  app.get("/filelist", (req, res) => {
-    contractService.getFileList().then(result => { res.send(result) })
-  });
 
-  app.get("/file", (req, res) => {
-    contractService.getFileHash(req.query.id).then(hashName => {
-      console.log(hashName)
-      ipfsService.downloadFile(hashName[0]).then(file => {
-        res.set('Content-disposition', 'attachment; filename=' + hashName[1]);
-        res.send(file)
-      })
-    })
-  });
-
-  app.delete("/file", (req, res) => {
-    console.log(req.query.id)
-    contractService.deleteElement(req.query.id).then(result => {
-      res.send(result);
-    })
-  });
-
-})
