df26bf85fa1b2ae540c6d7031eab1d03b6d58f19
constantin <constantin.ponfick@yahoo.de>
2019-01-17 11:02:53 +0100

    structure

--- coffee/src/app/coffee-information/coffee-information.component.ts
+++ coffee/src/app/coffee-information/coffee-information.component.ts
@@ -85,13 +85,14 @@ export class CoffeeInformationComponent implements OnInit {
     this.closeDialogService.sendMessage('Message from Coffee information component to dialog component!');
   }
 
+  
    async loading() {
     if (this.selectedRowIndex === -1) {
       window.alert('No coffee selected!');
     } else {
       this.coffeeInProcess = true;
       console.log('Going to while');
-      wait;
+      var wait = '0';
       while (wait === '0') {
         // this.httpService.sendData(this.trigger).subscribe(res => {
         // console.log(res);
