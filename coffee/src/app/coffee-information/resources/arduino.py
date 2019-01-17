886abbe4a4531094b854b0c42e0b6fa77495370f
constantin <constantin.ponfick@yahoo.de>
2019-01-17 10:19:06 +0100

    arduino

--- coffee/src/app/coffee-information/resources/arduino.py
+++ coffee/src/app/coffee-information/resources/arduino.py
@@ -5,23 +5,15 @@ import http
 import urllib.request
 
 
-ser = serial.Serial('/dev/ttyS0', 9600,timeout = 1000)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
-def request(path):
+def request(path,information):
     f = urllib.request.urlopen("http://localhost:8080/" + path)
     s = f.read().decode("utf-8")
     return s
-
-def setState(s):
-    if s == "1":
-        triggered = True
-    else:
-        triggered = False
-    return triggered
         
 while True:
     input = ser.readline()
-    
+    print(input)
     if ("Card UID:" in input.decode("utf-8") ):
-        print (ser.readline())
-    
+        print("test")
